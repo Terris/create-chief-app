abc0a06f12d050e2b42da40ead8325de9ef49013
Terris Kremer <terris.kremer@gmail.com>
2020-08-02 21:39:29 -0400

    add api url

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,15 +2,9 @@ import React from 'react'
 import { Layout } from 'src/layouts'
 
 export const HomePage = () => {
-  console.log(process.env)
   return (
     <Layout>
       <h1>Home Page</h1>
-      <p>
-        API URL:{' '}
-        {process.env.REACT_APP_API_URL ||
-          "You'll want to set your api url in .env."}
-      </p>
     </Layout>
   )
 }
